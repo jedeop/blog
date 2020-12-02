311041871a0cfd8175645e69282d73f924df1f40
jedeop <cong0116@naver.com>
2020-12-02 20:41:39 +0900

    add apollo ssr, styled-components ssr

--- client/src/client.tsx
+++ client/src/client.tsx
@@ -3,11 +3,25 @@ import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import { loadableReady } from '@loadable/component'
+import { ApolloClient, InMemoryCache, ApolloProvider, NormalizedCacheObject } from '@apollo/client'
+
+declare global {
+  interface Window {
+    __APOLLO_STATE__: NormalizedCacheObject;
+  }
+}
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache().restore(window.__APOLLO_STATE__)
+})
 
 loadableReady(() => {
   ReactDOM.hydrate((
+  <ApolloProvider client={client}>
     <BrowserRouter>
       <App />
     </BrowserRouter>
+  </ApolloProvider>
   ), document.getElementById('root'))
 })
--- client/src/server.tsx
+++ client/src/server.tsx
@@ -2,10 +2,14 @@ import Koa from 'koa'
 import mount from 'koa-mount'
 import React from 'react'
 import { renderToString } from 'react-dom/server'
+import { ServerStyleSheet } from 'styled-components'
 import { StaticRouter } from 'react-router-dom'
 import { Helmet } from 'react-helmet'
 import { ChunkExtractor } from '@loadable/server'
 import path from 'path'
+import fetch from 'cross-fetch'
+import { ApolloClient, ApolloProvider, createHttpLink, InMemoryCache } from '@apollo/client'
+import { getDataFromTree } from '@apollo/react-ssr'
 
 import App from './App'
 
@@ -20,20 +24,39 @@ const webStats = path.resolve(
 
 app.use(mount('/dist', require('koa-static')('dist/client')));
 
-app.use(ctx => {
+app.use(async ctx => {
+  const client = new ApolloClient({
+    ssrMode: true,
+    link: createHttpLink({
+      uri: 'http://localhost:8888/graphql',
+      credentials: 'same-origin',
+      headers: {
+        cookie: ctx.get('Cookie'),
+      },
+      fetch,
+    }),
+    cache: new InMemoryCache(),
+  })
   const context: {
     url?: string
   } = {}
-  
-  const webExtractor = new ChunkExtractor({ statsFile: webStats })
-  const jsx = webExtractor.collectChunks(
-    <StaticRouter location={ctx.req.url} context={context}>
-      <App />
-    </StaticRouter>
-  )
 
-  const content = renderToString(jsx)
-  
+  const webExtractor = new ChunkExtractor({ statsFile: webStats })
+  const sheet = new ServerStyleSheet()
+
+  const jsx = webExtractor.collectChunks(
+      <ApolloProvider client={client}>
+        <StaticRouter location={ctx.req.url} context={context}>
+          <App />
+        </StaticRouter>
+      </ApolloProvider>
+    )
+
+  await getDataFromTree(jsx)
+
+  const content = renderToString(sheet.collectStyles(jsx))
+
+  const initialState = client.extract()
   const helmet = Helmet.renderStatic()
 
   const html = `
@@ -46,10 +69,11 @@ app.use(ctx => {
             ${helmet.meta.toString()}
             ${helmet.link.toString()}
             ${webExtractor.getLinkTags()}
-            ${webExtractor.getStyleTags()}
+            ${sheet.getStyleTags()}
         </head>
         <body ${helmet.bodyAttributes.toString()}>
           <div id="root">${content}</div>
+          <script type="text/javascript">window.__APOLLO_STATE__=${JSON.stringify(initialState).replace(/</g, '\\u003c')};</script>
           ${webExtractor.getScriptTags()}
         </body>
     </html>
