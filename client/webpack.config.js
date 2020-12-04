372fccae48163b5c2701c7d0530c82ff9ca7e8a5
jedeop <cong0116@naver.com>
2020-12-04 22:09:30 +0900

    merge client/server webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -1,1 +1,45 @@
-module.exports = env => require(`./webpack.${env.MODE}.js`)
+const path = require('path');
+const nodeExternals = require('webpack-node-externals');
+const LoadablePlugin = require('@loadable/webpack-plugin')
+
+
+const getConfig = (mode) => {
+  const isDevMode = process.env.NODE_ENV !== 'production'
+  const isServer = mode == 'server'
+
+  return {
+    name: mode,
+    mode: isDevMode ? 'production' : 'development',
+    target: isServer ? 'node' : 'web',
+    node: isServer ? false : undefined,
+    watch: isDevMode,
+    entry: `./src/${mode}.tsx`,
+    output: {
+      filename: '[name].js',
+      path: path.resolve(__dirname, `dist/${mode}/`),
+      publicPath: `/dist/`,
+    },
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          exclude: /node_modules/,
+          use: ['babel-loader', 'ts-loader'],
+        },
+      ]
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src')
+      }
+    },
+    plugins: isServer ? [] : [new LoadablePlugin()],
+    externals: isServer ? [nodeExternals()] : [],
+  }
+}
+
+module.exports = [
+  getConfig('client'),
+  getConfig('server'),
+]
