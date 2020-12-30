5240985280feb70ed6841ee924c4be339e843c05
jedeop <cong0116@naver.com>
2020-12-30 19:45:58 +0900

    split header as component, add links

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import { Link, Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import loadable from '@loadable/component'
 import { Helmet } from 'react-helmet'
 import styled from 'styled-components'
 
-import Logo from '@/components/logo'
 import GlobalStyle from '@/styles/global'
+import Header from './components/header'
 
 const HomePage = loadable(() => import('@/pages/HomePage'))
 const PostPage = loadable(() => import('@/pages/PostPage'))
@@ -14,22 +14,6 @@ const Container = styled.div`
   min-height: 100vh;
 `
 
-const Header = styled.header`
-  padding: 5px 15px;
-  background: linear-gradient(272deg, #61CDB7 0%, #24B799 100%);
-  font-size: 25px;
-  font-weight: bold;
-  color: white;
-`
-
-const FlexLink = styled(Link)`
-  display: flex;
-  align-items: center;
-`
-
-const Title = styled.span`
-  margin-left: 5px;
-`
 
 const App = () => (
   <Container>
@@ -39,12 +23,7 @@ const App = () => (
       <link rel="icon" href="/favicon.ico" />
       <link rel="stylesheet" href="/dist/fonts/css/ibm-plex-sans-kr.min.css"/>
     </Helmet>
-    <Header>
-      <FlexLink to="/">
-        <Logo width={100} />
-        <Title>블로그</Title>
-      </FlexLink>
-    </Header>
+    <Header />
     <main>
       <Switch>
         <Route exact path="/" render={() => <HomePage />} />
---
+++ frontend/src/components/header.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import styled from 'styled-components'
+import Logo from './logo'
+import { Edit3, LogIn } from 'react-feather'
+
+const Container = styled.header`
+  padding: 5px 15px;
+  background: linear-gradient(272deg, #61CDB7 0%, #24B799 100%);
+  font-size: 25px;
+  font-weight: bold;
+  color: white;
+
+  display: flex;
+`
+const FlexLink = styled(Link)`
+  display: flex;
+  align-items: center;
+`
+const Title = styled.span`
+  margin-left: 5px;
+`
+const LeftLink = styled(FlexLink)`
+  margin-right: auto;
+`
+const RightLink = styled(FlexLink)`
+  margin: 0px 5px;
+`
+
+export default function Header() {
+  return (
+    <Container>
+      <LeftLink to="/">
+        <Logo width={100} />
+        <Title>블로그</Title>
+      </LeftLink>
+      <RightLink to="/write">
+        <Edit3 size={25} />
+      </RightLink>
+      <RightLink to="/write">
+        <LogIn size={25} />
+      </RightLink>
+    </Container>
+  )
+}
